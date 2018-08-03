8aa59c16088cb025121101fe989b89a19fbad2a2
Anne <anne.mullin@uvm.edu>
2018-08-03 14:10:39 -0400

    R code Aug 3
    testing github push

---
+++ re_As.R
@@ -0,0 +1,14 @@
+hist(F$re_AsM2T)
+fivenum(F$re_AsM2T)
+mean(F$re_AsM2T, na.rm = TRUE)
+sd(F$re_AsM2T, na.rm = TRUE)
+
+fivenum(DT$re_AsM00)
+mean(F$re_AsM00)
+sd(F$re_AsM00)
+
+summary(F$t212T)
+
+fivenum(F$re_AsC00)
+fivenum(F$re_AsM00)
+fivenum(F$re_AsM2T)
